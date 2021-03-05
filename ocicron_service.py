efa552d6255998c562b8c51776d9b2e5ab7ff683
Dave Franco <davefranco1987@gmail.com>
2021-03-05 09:28:42 -0300

    add database ops

--- ocicron_service.py
+++ ocicron_service.py
@@ -22,20 +22,25 @@ class OCI:
             if self.region is not None:
                 self.compute = oci.core.ComputeClient(config={'region':self.region}, signer=signer)
                 self.identity = oci.identity.IdentityClient(config={'region':self.region}, signer=signer)
+                self.database = oci.database.DatabaseClient(config={'region':self.region}, signer=signer)
             else:
                 self.compute = oci.core.ComputeClient(config={}, signer=signer)
                 self.identity = oci.identity.IdentityClient(config={}, signer=signer)
+                self.database = oci.database.DatabaseClient(config={}, signer=signer)
         elif self.auth_type == "config":
             self.config = oci.config.from_file(file_location=config_file, profile_name=profile)
             if self.region is not None:
                 self.config['region'] = self.region
             self.compute = oci.core.ComputeClient(self.config)
             self.identity = oci.identity.IdentityClient(self.config)
+            self.database = oci.database.DatabaseClient(self.config)
         else:
             raise Exception("Unrecognize authentication type: auth_type=(principal|config)")
         
         self.compartment_ids = []
         self.compute_instances = []
+        self.db_systems = []
+        self.db_nodes = []
     
     def _get_sub_compartment_ids(self, cid):
         
@@ -81,19 +86,28 @@ class OCI:
                     self.compute_instances.append(vm)         
         return self.compute_instances
     
-    def filter_by_tags(self, tags):
+    def filter_by_tags(self, tags, service='compute'):
         """
         returns list if OCID of a given tags
         tags = {"Stop":"20","Start": "08","Weekly_stop":"Yes"}
         """
-        vmOCID=[]
-        for vm in self.compute_instances:
-            #compare dictionary and length should be the same
-            if len(tags.items() & vm.freeform_tags.items()) == len(tags.items()):
-                vmOCID.append(vm.id)
-        return vmOCID
+        OCIDS=[]
+
+        if service == 'compute':
+            for vm in self.compute_instances:
+                #compare dictionary and length should be the same
+                if len(tags.items() & vm.freeform_tags.items()) == len(tags.items()):
+                    OCIDS.append(vm.id)
+        elif service == 'database':
+            for db in self.db_systems:
+                if len(tags.items() & db.freeform_tags.items()) == len(tags.items()):
+                    OCIDS.append(db.id)
+        else:
+            raise Exception("Unrecognize service: either compute or database are acccepted")
+        
+        return OCIDS
     
-    def _discover_tags(self, tag_keys=TAG_KEYS):
+    def _discover_tags(self, tag_keys=TAG_KEYS, service='compute'):
         """
         Discovery tag keys and values from compute freeform_tags
 
@@ -101,16 +115,26 @@ class OCI:
         result: [{'Start': '08', 'Stop': '20', 'Weekly_stop': 'No'}, {'Start': '08', 'Stop': '20', 'Weekly_stop': 'Yes'}]
         """
         result = []
-        vm_group = {}
-        for vm in self.compute_instances:
-            if len(tag_keys & vm.freeform_tags.keys()) == len(tag_keys):
-                tags = {}
-                for key in tag_keys:
-                    tags[key] = vm.freeform_tags[key]
-                if tags not in result:
-                    result.append(tags)
-        return result
+        if service == 'compute':
+            for vm in self.compute_instances:
+                if len(tag_keys & vm.freeform_tags.keys()) == len(tag_keys):
+                    tags = {}
+                    for key in tag_keys:
+                        tags[key] = vm.freeform_tags[key]
+                    if tags not in result:
+                        result.append(tags)
+        elif service == 'database':
+            for db in self.db_systems:
+                if len(tag_keys & db.freeform_tags.keys()) == len(tag_keys):
+                    tags = {}
+                    for key in tag_keys:
+                        tags[key] = db.freeform_tags[key]
+                    if tags not in result:
+                        result.append(tags)
+        else:
+            raise Exception("Unrecognize service: either compute or database are acccepted")
 
+        return result
 
     def vms_by_tags(self, tag_keys=TAG_KEYS):   
 
@@ -130,12 +154,73 @@ class OCI:
         for ocid in instances:
             self.compute.instance_action(ocid, action)
 
+    def get_all_dbsystems(self):
+        """
+        Return all dbsystems in a given compartment
+        """
+        if len(self.compartment_ids) <= 0:
+            return
+
+
+        for compartment_id in self.compartment_ids:   
+            response = self.database.list_db_systems(
+                compartment_id=compartment_id,
+                sort_by="TIMECREATED",
+                sort_order="ASC"
+            )
+            #list databse system with pagination
+            dbsys = response.data
+            while response.has_next_page:
+                response = self.database.list_db_systems(compartment_id, page=response.next_page)
+                dbsys.extend(response.data)  
+
+            #Store Database system ids
+            for dbs in dbsys:
+                if dbs.lifecycle_state == 'AVAILABLE':
+                    self.db_systems.append(dbs)    
+                         
+        return self.db_systems
+    
+    def get_all_db_nodes(self, db_system_ids):
+        """
+        Return all DB Nodes in a given compartment
+        """
+        if len(db_system_ids) <= 0:
+            return self.db_nodes
+
+        for compartment_id in self.compartment_ids:
+            for ocid in db_system_ids:
+                response = self.database.list_db_nodes(
+                    compartment_id=compartment_id,
+                    db_system_id=ocid)
+                self.db_nodes.extend(response.data)        
+        return self.db_nodes
+    
+    def dbs_by_tags(self, tag_keys=TAG_KEYS):   
+
+        tags = self._discover_tags(service='database')
+        result = []
+        for tag in tags:
+            db_group = {}
+            db_group["tags"] = tag
+            db_group["dbOCID"] = self.filter_by_tags(tag, service='database')
+            result.append(db_group)
+        return result
+
+    def database_action(self, db_node_ids, action):
+        """
+        Perform action of a given list of db nodes OCID
+        """
+        for ocid in db_node_ids:
+            self.database.db_node_action(ocid, action)
+
 class ScheduleDB:
 
     def __init__(self, location=os.path.join(DEFAULT_LOCATION, DB_FILE_NAME)):
         self.location = location
         self.db = TinyDB(self.location)
         self.vm_table = self.db.table('vms')
+        self.dbsys_table = self.db.table('db')
         self.cid_table = self.db.table('compartments')
         self.cron_table = self.db.table('cron')
 
